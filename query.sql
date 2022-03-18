6f8e3dbc2bd0e42d877a6661905df1685f57fd2f
Sam <sam@eatpos.co.uk>
2022-03-18 17:56:41 +0000

    next

--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
 SELECT b.id,
-       c.name,
-       b.name,
+       c.name as 'Company name',
+       b.name as 'Branch name',
        IF(Date_format(s.visit_datetime, '%m-%y') IS NOT NULL,
-       Date_format(s.visit_datetime, '%m-%y'), 'N/A') AS month_data,
+       Date_format(s.visit_datetime, '%m-%y'), 'N/A') AS 'Month-Year',
        SUM(CASE
              WHEN s.social_type_id = 1 THEN 1
              ELSE 0
@@ -32,11 +32,14 @@ SELECT b.id,
 FROM   survey_responses s
        right outer join branches b
                      ON s.branch_id = b.id
-                        AND social_raw_score IS NOT NULL AND b.status = 1 AND brand_site = 0 AND s.survey_mode_id = 8 AND s.status IN (1,2,5)
+                        AND social_raw_score IS NOT NULL AND s.survey_mode_id = 8 AND s.status IN (1,2,5)
        join companies c
-         ON c.id = b.company_id AND c.status =1 
+         ON c.id = b.company_id AND c.status =1 #
+WHERE b.status = 1 AND brand_site = 0
 GROUP  BY branch_id,
-          Date_format(s.visit_datetime, '%m-%y');
+          Date_format(s.visit_datetime, '%m-%y')
+          ORDER BY branch_id ASC,
+          Date_format(s.visit_datetime, '%m-%y') ASC;
 
 /*
 
